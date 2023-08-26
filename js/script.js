17085a2d539b36a383fb119dce567b3a48b2c8d3
Brayan17king <xbrayansneyder@gmail.com>
2023-08-25 20:19:50 -0500

    feat:✨ creacion de archivo Js para la funcionalidad.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Selecciona el elemento con la clase "menu-icon" y lo almacena en la variable "menuIcon"
+let menuIcon = document.querySelector('.menu-icon');
+
+// Selecciona el elemento con la clase "sidebar" y lo almacena en la variable "sidebar"
+let sidebar = document.querySelector('.sidebar');
+
+// Se define un evento onclick para el elemento "menuIcon"
+menuIcon.onclick = function() {
+    // En el momento en que se hace clic en "menuIcon", se ejecuta esta función
+    // La función toggle agrega la clase "small-slidebar" si no está presente, y la elimina si está presente.
+    sidebar.classList.toggle('small-slidebar');  
+}
